e05a99301c09a6806aabdb7f045c86309a02e4eb
ndbac <ryannguyen0303@gmail.com>
2024-02-11 13:09:49 +0700

    chore: init repo

---
+++ src/config/theme/palette-base.ts
@@ -0,0 +1,18 @@
+import { PaletteOptions } from '@mui/material'
+
+const paletteBase: Partial<PaletteOptions> = {
+  primary: {
+    light: '#25bbff',
+    main: '#4f56fd',
+    dark: '#3e2eff',
+    contrastText: '#fbfbfb',
+  },
+  secondary: {
+    light: '#FDE6A8',
+    main: '#ffaf35',
+    dark: '#D0821C',
+    contrastText: '#fbfbfb',
+  },
+}
+
+export default paletteBase
