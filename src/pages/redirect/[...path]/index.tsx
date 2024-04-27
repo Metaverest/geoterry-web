e37b7179bbb803e944960c56bd7eb5e3a121e895
ndbac <ryannguyen0303@gmail.com>
2024-04-27 14:14:32 +0800

    feat: handle deeplink to checkly app

---
+++ src/pages/redirect/[...path]/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+import { NextPageWithLayout } from '@/interfaces/layout'
+import { MainLayout } from '@/components/layout'
+import { useRouter } from 'next/router'
+import isString from 'lodash/isString'
+import isArray from 'lodash/isArray'
+
+const DynamicHomeFeature = dynamic(() => import('../../../components/home/feature'))
+
+const Redirect: NextPageWithLayout = () => {
+  const router = useRouter()
+  const { path } = router.query
+
+  useEffect(() => {
+    const handleDeepLink = (path: string): void => {
+      window.location.href = `checkly://${path}`
+    }
+
+    if (isArray(path)) {
+      handleDeepLink(`${path.join('/')}`)
+    }
+
+    if (isString(path)) {
+      handleDeepLink(`${path}`)
+    }
+  }, [path, router])
+
+  return (
+    <>
+      <DynamicHomeFeature />
+    </>
+  )
+}
+
+Redirect.getLayout = (page) => <MainLayout excludeHeaderFooter>{page}</MainLayout>
+
+export default Redirect
