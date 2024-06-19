b215b9bdd8b7f565b6e6d9c0a1e0502b54a04fba
Ben Nguyen <66194265+ndbac@users.noreply.github.com>
2024-06-19 22:47:52 +0800

    feat: add home page (#4)

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          100: '#FBFBFB',
+          200: '#EAEAEA',
+          300: '#DFDFDF',
+          400: '#999999',
+          500: '#7F7F7F',
+          600: '#666666',
+          700: '#4C4C4C',
+          800: '#333333',
+          900: '#191919',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
